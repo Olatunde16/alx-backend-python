6d5184cfef93ed351a648138011406f3c40229e4
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-07-09 14:34:30 +0000

    Tasks Successful

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 '''
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''Generates a sequence of 10 numbers.
     '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
